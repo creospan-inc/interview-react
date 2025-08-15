a764154a923e59c012b6276e047530217fb29414
Dave Krause <github@idk.email>
2025-08-15 14:02:49 -0400

    challenge 2 - missed this on earlier commit - create_user call must conform to the type.  only admin and reguar user have name and email properties

--- src/challenge1/UserDisplay.tsx
+++ src/challenge1/UserDisplay.tsx
@@ -1,20 +1,19 @@
-import {UserDisplayComponent} from "./types";
-import {create_user} from "./service";
+import { UserDisplayComponent } from "./types";
+import { create_user } from "./service";
 
 const UserDisplay: UserDisplayComponent = (user) => {
     const onClick = () => {
         // imagine in a magic land this calls the server and refreshes the user session
-        create_user({
-            id: Date.now().toString(),
-            name: 'John',
+        const id = create_user({
+            name: 'John Smith',
             type: 'regular',
             email: 'john@doe.com'
         });
     };
 
-    if(user.name && user.email){
+    if (user.type === 'admin' || user.type === 'regular' && user.name && user.email) {
         return <div>Hello {user.name} ({user.email})!</div>;
-    }else{
+    } else {
         return <div onClick={onClick}>Register Now!</div>;
     }
 };
